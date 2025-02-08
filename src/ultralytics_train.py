800ae0c924297fd32be5e72b4b977226a4ecd58e
siddsrrm <siddsrrm@gmail.com>
2025-02-08 20:22:59 +0000

    Trained model Task 1

--- src/ultralytics_train.py
+++ src/ultralytics_train.py
@@ -4,4 +4,4 @@ from ultralytics import YOLO
 model = YOLO("yolo11n.pt")  # load a pretrained model (recommended for training)
 
 # Train the model
-results = model.train(data="VisDrone.yaml", epochs=100, imgsz=640)
+results = model.train(data="VisDrone.yaml", epochs=5, imgsz=640)
